38b07c9c6a5c738ebbde8069c3ec98ea5ecf40c8
Corey Hammond <coreyjhammond@gmail.com>
2021-02-19 11:10:40 -0800

    configured table

--- components/table/table.js
+++ components/table/table.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useTable } from 'react-table';
+import styles from './table.module.scss';
 
 function Table({ columns, data }) {
   const {
@@ -11,22 +12,12 @@ function Table({ columns, data }) {
   } = useTable({ columns, data });
 
   return (
-    <table {...getTableProps()} style={{ border: 'solid 1px blue' }}>
+    <table className={styles.FirstTable} {...getTableProps()}>
       <thead>
         {headerGroups.map((headerGroup) => (
           <tr {...headerGroup.getHeaderGroupProps()}>
             {headerGroup.headers.map((column) => (
-              <th
-                {...column.getHeaderProps()}
-                style={{
-                  borderBottom: 'solid 3px red',
-                  background: 'aliceblue',
-                  color: 'black',
-                  fontWeight: 'bold',
-                }}
-              >
-                {column.render('Header')}
-              </th>
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
             ))}
           </tr>
         ))}
@@ -37,18 +28,7 @@ function Table({ columns, data }) {
           return (
             <tr {...row.getRowProps()}>
               {row.cells.map((cell) => {
-                return (
-                  <td
-                    {...cell.getCellProps()}
-                    style={{
-                      padding: '10px',
-                      border: 'solid 1px gray',
-                      background: 'papayawhip',
-                    }}
-                  >
-                    {cell.render('Cell')}
-                  </td>
-                );
+                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
               })}
             </tr>
           );
--- pages/tables/first-table.js
+++ pages/tables/first-table.js
@@ -1,40 +1,39 @@
 import React from 'react';
-
-import { useTable } from 'react-table';
 import Table from '../../components/table/table';
 
 export default function App() {
-  const data = React.useMemo(
-    () => [
-      {
-        col1: 'Hello',
-        col2: 'World',
-      },
-      {
-        col1: 'react-table',
-        col2: 'rocks',
-      },
-      {
-        col1: 'whatever',
-        col2: 'you want',
-      },
-    ],
-    []
-  );
+  const columns = React.useMemo(() => [
+    {
+      Header: 'License',
+      accessor: 'license',
+    },
+    {
+      Header: 'Invoice',
+      accessor: 'invoice',
+    },
+    {
+      Header: 'Purchase Date',
+      accessor: 'purchaseDate',
+    },
+  ]);
 
-  const columns = React.useMemo(
-    () => [
-      {
-        Header: 'Column 1',
-        accessor: 'col1', // accessor is the "key" in the data
-      },
-      {
-        Header: 'Column 2',
-        accessor: 'col2',
-      },
-    ],
-    []
-  );
+  const data = React.useMemo(() => [
+    {
+      license: '2PC-XCG',
+      invoice: 85556,
+      purchaseDate: '02/04/2009',
+    },
+    {
+      license: '47R-177',
+      invoice: 19418,
+      purchaseDate: '07/12/2005',
+    },
+    {
+      license: '496-D35',
+      invoice: 19710,
+      purchaseDate: '08/10/2005',
+    },
+  ]);
 
   return <Table columns={columns} data={data} />;
 }
